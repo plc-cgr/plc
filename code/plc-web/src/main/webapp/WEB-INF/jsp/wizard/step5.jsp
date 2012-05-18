1e377c06737fc75aa76dc2e5f1d8752b1e6a12aa
Abraham Evans-EL <aevansel@5amsolutions.com>
2012-05-18 17:58:13 -0400

    PLC-80: Tweaking video javascript in hopes of fixing problems on prod.

--- code/plc-web/src/main/webapp/WEB-INF/jsp/wizard/step5.jsp
+++ code/plc-web/src/main/webapp/WEB-INF/jsp/wizard/step5.jsp
@@ -2,8 +2,7 @@
 <div id="video_form">
     <div class="well">
         <iframe id="ctrvideo"
-            src="http://player.vimeo.com/video/37704392?title=0&amp;byline=0&amp;portrait=0&amp;api=1&amp;player_id=ctrvideo"
-            width="500" height="375" frameborder="0"></iframe>
+            src="http://player.vimeo.com/video/37704392?api=1&player_id=ctrvideo&title=0&byline=0&portrait=0" width="500" height="375" frameborder="0"></iframe>
     </div>
 
     <div class="alert-message" id="video_instructions">You must finish watching the video to advance in the
--- code/plc-web/src/main/webapp/scripts/plc-wizard.js
+++ code/plc-web/src/main/webapp/scripts/plc-wizard.js
@@ -19,30 +19,17 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 function disableNavigationUntilVideoFinishes() {
-	var video = $('#ctrvideo')[0];
-	$f(video).addEvent('ready', ready);
+	var iframe = $('#ctrvideo')[0];
+	var player = $f(iframe);
+	player.addEvent('ready', function() {
+	    player.addEvent('finish', onFinish);
+	});
 
-	function addEvent(element, eventName, callback) {
-		if (element.addEventListener) {
-			element.addEventListener(eventName, callback, false);
-		}
-		else {
-			element.attachEvent(eventName, callback, false);
-		}
-	}
-
-	function ready(player_id) {
-		var player = $f(player_id);
-
-		function onFinish() {
-			player.addEvent('finish', function(data) {
-				$(":submit").removeClass('disabled');
-				$(":submit").removeAttr('disabled');
-				$("#video_instructions").text("Click Next to continue the consent process.");
-				$("#video_instructions").addClass("success");
-			});
-		}
-		onFinish();
+	function onFinish() {
+	    $(":submit").removeClass('disabled');
+	    $(":submit").removeAttr('disabled');
+	    $("#video_instructions").text("Click Next to continue the consent process.");
+	    $("#video_instructions").addClass("success");
 	}
 }
 
